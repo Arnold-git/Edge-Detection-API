ef0a592dc4c7edf655bd0951a311adc8c3071175
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-01-21 14:03:25 +0100

    modified app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
 
     uploaded_file = request.files.get("file")
